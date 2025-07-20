e12aab59be64a8c95362c878ff9caaa87b571450
yan <hellowwangyan@163.com>
2025-07-20 10:07:42 +0800

    feat: 实现配置功能

--- cmd/mb-apiserver/app/server.go
+++ cmd/mb-apiserver/app/server.go
@@ -1,13 +1,21 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
+	"github.com/yanking/miniblog/cmd/mb-apiserver/app/options"
 
 	"github.com/spf13/cobra"
 )
 
+var configFile string // 配置文件路径
+
 // NewMiniBlogCommand 创建一个 *cobra.Command 对象，用于启动应用程序.
 func NewMiniBlogCommand() *cobra.Command {
+	// 创建默认的应用命令行选项
+	opts := options.NewServerOptions()
+
 	cmd := &cobra.Command{
 		// 指定命令的名字，该名字会出现在帮助信息中
 		Use: "mb-apiserver",
@@ -42,12 +50,36 @@ The project features include:
 		SilenceUsage: true,
 		// 指定调用 cmd.Execute() 时，执行的 Run 函数
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Hello MiniBlog!")
+			// 将 viper 中的配置解析到选项 opts 变量中.
+			if err := viper.Unmarshal(opts); err != nil {
+				return err
+			}
+
+			// 对命令行选项值进行校验.
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+
+			fmt.Printf("ServerMode from ServerOptions: %s\n", opts.JWTKey)
+			fmt.Printf("ServerMode from Viper: %s\n\n", viper.GetString("jwt-key"))
+
+			jsonData, _ := json.MarshalIndent(opts, "", "  ")
+			fmt.Println(string(jsonData))
+
 			return nil
 		},
 		// 设置命令运行时的参数检查，不需要指定命令行参数。例如：./miniblog param1 param2
 		Args: cobra.NoArgs,
 	}
 
+	// 初始化配置函数，在每个命令运行时调用
+	cobra.OnInitialize(onInitialize)
+
+	// cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	// 推荐使用配置文件来配置应用，便于管理配置项
+	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", filePath(), "Path to the miniblog configuration file.")
+
+	// 将 ServerOptions 中的选项绑定到命令标志
+	opts.AddFlags(cmd.PersistentFlags())
 	return cmd
 }
