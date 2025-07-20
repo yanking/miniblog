ffa0046bd9757b5191b493b472ae278ff54a5073
yan <hellowwangyan@163.com>
2025-07-20 15:08:56 +0800

    feat: 支持优雅关停功能

--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -1,8 +1,12 @@
 package apiserver
 
 import (
+	"context"
 	"github.com/yanking/miniblog/internal/pkg/log"
 	"github.com/yanking/miniblog/internal/pkg/server"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	genericoptions "github.com/yanking/miniblog/pkg/options"
@@ -81,7 +85,27 @@ func (cfg *Config) NewUnionServer() (*UnionServer, error) {
 
 // Run 运行应用.
 func (s *UnionServer) Run() error {
-	s.srv.RunOrDie()
+	go s.srv.RunOrDie()
+
+	// 创建一个 os.Signal 类型的 channel，用于接收系统信号
+	quit := make(chan os.Signal, 1)
+	// 当执行 kill 命令时（不带参数），默认会发送 syscall.SIGTERM 信号
+	// 使用 kill -2 命令会发送 syscall.SIGINT 信号（例如按 CTRL+C 触发）
+	// 使用 kill -9 命令会发送 syscall.SIGKILL 信号，但 SIGKILL 信号无法被捕获，因此无需监听和处理
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// 阻塞程序，等待从 quit channel 中接收到信号
+	<-quit
+
+	log.Infow("Shutting down server ...")
+
+	// 优雅关闭服务
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// 先关闭依赖的服务，再关闭被依赖的服务
+	s.srv.GracefulStop(ctx)
+
+	log.Infow("Server exited")
 	return nil
 }
 
